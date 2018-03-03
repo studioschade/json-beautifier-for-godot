55030e516c9a1b2b88c8210a9fa77d8867941d74
Michael Alexsander Silva Dias <michaelalexsander@protonmail.com>
2018-03-03 15:13:33 -0300

    Insertion of the license in the file and changed the function's explanation to be an actual comment.

--- addons/json_beautifier/json_beautifier.gd
+++ addons/json_beautifier/json_beautifier.gd
@@ -1,10 +1,32 @@
+#-----------------------------------------------------------------------------#
+# JSON Beautifier                                                             #
+# Copyright (c) 2018 Michael Alexsander Silva Dias                            #
+#                                                                             #
+# Permission is hereby granted, free of charge, to any person obtaining a     #
+# copy of this software and associated documentation files (the "Software"),  #
+# to deal in the Software without restriction, including without limitation   #
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,    #
+# and/or sell copies of the Software, and to permit persons to whom the       #
+# Software is furnished to do so, subject to the following conditions:        #
+#                                                                             #
+# The above copyright notice and this permission notice shall be included in  #
+# all copies or substantial portions of the Software.                         #
+#                                                                             #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,    #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER      #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING     #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER         #
+# DEALINGS IN THE SOFTWARE.                                                   #
+#-----------------------------------------------------------------------------#
+
 extends Node
 
+# Takes valid JSON (if invalid, it will return a error according with Godot's
+# 'validade_json' method) and a number of spaces for indentation (Default is
+# '0', in which it will use tabs instead)
 static func beautify_json(json, spaces = 0):
-	""" JSON beautifier. Takes valid JSON (if invalid, it will return a error
-	according with Godot's 'validade_json' method) and a number of spaces for
-	indentation (Default is '0', in which it will use tabs instead) """
-	
 	var error_message = validate_json(json)
 	if not error_message.empty():
 		return error_message
